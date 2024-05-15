0331d09c31240200c789498261a7a32801df0fbb
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-05-15 17:30:35 +0200

    10 mail  tutte diverse

--- JS/main.js
+++ JS/main.js
@@ -2,38 +2,37 @@ const { createApp } = Vue
 createApp({
     data() {
         return {
-            titolo: "vue-email-list",
+            titolo: "VUE GENERATE 10 EMAIL",
             ul: "Le mail generate sono:",
             element: "",
             email: [],
         }
     },
 
+
+
     methods: {
         getEmail() {
-            axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
-                for (let i = 0; i <= 9; i++) {
-                    // this.email.push(this.element)
+            for (let i = 0; i <= 9; i++) {
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
                     this.element = result.data.response;
-
-
                     this.email.push(this.element)
 
+                    // console.log(this.email)
+                })
+            }
+        },
 
-                }
-                console.log(this.email)
-            })
-
-
-        }
 
     },
+
 
 
     //MOMENTI DI BASE
     mounted() {
         console.log("App montata")
         this.getEmail()
-
     }
+
+
 }).mount("#app")
