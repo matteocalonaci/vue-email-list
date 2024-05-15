0aaa1bed5766abf0bd24f44060aa450d6b8ab727
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-05-15 17:01:27 +0200

    Generati 10 indirizzi email stampati in pagina.

--- JS/main.js
+++ JS/main.js
@@ -3,14 +3,25 @@ createApp({
     data() {
         return {
             titolo: "vue-email-list",
-
+            ul: "Le mail generate sono:",
+            element: "",
+            email: [],
         }
     },
 
     methods: {
         getEmail() {
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result) => {
+                for (let i = 0; i <= 9; i++) {
+                    // this.email.push(this.element)
+                    this.element = result.data.response;
 
+
+                    this.email.push(this.element)
+
+
+                }
+                console.log(this.email)
             })
 
 
@@ -22,7 +33,7 @@ createApp({
     //MOMENTI DI BASE
     mounted() {
         console.log("App montata")
-
+        this.getEmail()
 
     }
 }).mount("#app")
